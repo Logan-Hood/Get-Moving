fc80b8275a869fff977bce5e624cb7ad27f4581d
Logan-Hood <logan.hood42@gmail.com>
2024-11-13 16:09:41 -0500

    upload 3

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function goToSetup() {
   window.location.href = 'setup.html';
 }
 
-
 function goToListing() {
     window.location.href = 'listing.html';
 }
@@ -14,4 +13,37 @@ function goToListing() {
 
 function goBackToSetup() {
     window.location.href = 'setup.html';
+}
+
+function goBackToListing() {
+  window.location.href = 'listing.html';
+}
+
+function goToProfile1() {
+  window.location.href ='profile.html';
+}
+
+
+function goToMoney() {
+  window.location.href ='money.html';
+}
+
+function goToGear() {
+  window.location.href ='gear.html';
+}
+
+function goToVolunteer() {
+  window.location.href ='volunteer.html';
+}
+
+function goBackToProfile1() {
+  window.location.href = 'profile.html';
+}
+
+function goToPayment() {
+  window.location.href ='payment.html';
+}
+
+function goBackToMoney() {
+  window.location.href = 'money.html';
 }
