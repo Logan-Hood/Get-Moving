72d3130309006b73272bd314952dbd7897dd260f
Logan-Hood <logan.hood42@gmail.com>
2024-11-13 13:26:20 -0500

    upload 2

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function goToSetup() {
+  window.location.href = 'setup.html';
+}
+
+
+function goToListing() {
+    window.location.href = 'listing.html';
+}
+
+
+  function goBackToLogin() {
+    window.location.href = 'index.html';
+}
+
+function goBackToSetup() {
+    window.location.href = 'setup.html';
+}
